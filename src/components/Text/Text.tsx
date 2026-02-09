d8be98e3e998b8b2d73b5845f76da07ef1454ccc
kokk <kokk7310@gmail.com>
2026-02-09 22:44:12 +0900

    [feat]Text 컴포넌트 및 스타일 추가

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,38 @@
+import type { HTMLAttributes } from "react";
+import type { TextVariants } from "./Text.type";
+
+import * as S from "./Text.styled";
+
+export interface TextProps extends HTMLAttributes<HTMLElement> {
+  textType: TextVariants;
+  isInline?: boolean;
+}
+
+const TAG_MAP = {
+  heading: "h1",
+  title: "h2",
+  subTitle: "h3",
+  body: "p",
+  bodyBold: "p",
+  detail: "span",
+  detailBold: "span",
+} as const;
+
+const Text = ({
+  children,
+  textType = "body",
+  isInline,
+  ...attributes
+}: TextProps) => {
+  return (
+    <S.Text
+      as={TAG_MAP[textType]}
+      $textType={textType}
+      $isInline={isInline}
+      {...attributes}
+    >
+      {children}
+    </S.Text>
+  );
+};
+export default Text;
