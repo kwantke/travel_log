f172001007f8bd7d8aef947607cca3da96e71d4e
kokk <kokk7310@gmail.com>
2026-02-12 20:38:40 +0900

    [feat] Avatar 컴포넌트 및 스타일 추가

---
+++ src/components/common/AvatarCircle/AvatarCircle.tsx
@@ -0,0 +1,44 @@
+import useImageError from "@hooks/useImageError";
+import type { AvatarCircleSize } from "./AvatarCircl.type";
+import * as S from "./AvatarCircle.styled";
+
+interface AvartarCircleProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  size?: AvatarCircleSize;
+  profileImageUrl?: string;
+}
+
+const AvatarCircle = ({
+  size = "small",
+  profileImageUrl,
+  ...props
+}: AvartarCircleProps) => {
+  const { imageError, handleImageError } = useImageError({
+    imageUrl: profileImageUrl,
+  });
+  return (
+    <S.AvatarCircleContainer $size={size}>
+      {!imageError ? (
+        <img src={profileImageUrl} onError={handleImageError} {...props} />
+      ) : (
+        <S.FallbackIcon $size={size}>
+          <svg
+            width="11"
+            height="11"
+            viewBox="0 0 11 11"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="5.5" cy="2.5" r="2.5" fill="white" />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M10.9552 11H0C0.252614 8.19675 2.60856 6 5.47758 6C8.3466 6 10.7025 8.19675 10.9552 11Z"
+              fill="white"
+            />
+          </svg>
+        </S.FallbackIcon>
+      )}
+    </S.AvatarCircleContainer>
+  );
+};
+export default AvatarCircle;
