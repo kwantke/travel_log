675e1ec6c036efef46efca6ece6b2396001a385f
kokk <kokk7310@gmail.com>
2026-02-26 03:31:01 +0900

    [fix] 통합 테스트을 경우 TestJpaAuditing 설정 제거

--- backend/src/test/java/kr/tour/global/fixture/IntegrationTest.java
+++ backend/src/test/java/kr/tour/global/fixture/IntegrationTest.java
@@ -1,12 +1,9 @@
 package kr.tour.global.fixture;
 
 import kr.tour.TourApplication;
-import kr.tour.global.config.TestJpaAuditingConfig;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-@Import({TestJpaAuditingConfig.class})
 @ActiveProfiles("test")
 @SpringBootTest(
         classes = TourApplication.class,
