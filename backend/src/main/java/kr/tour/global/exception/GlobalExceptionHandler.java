9ee61c1084db54a2001c134b7d7efc224d646a11
kokk <kokk7310@gmail.com>
2026-02-28 02:24:40 +0900

    [feat] GlobalExceptionHandler 예외 처리 추가

--- backend/src/main/java/kr/tour/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/kr/tour/global/exception/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,7 +14,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(CoreException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequestException(CoreException exception) {
         log.warn("CoreException :: message = {}", exception.getMessage());
@@ -56,4 +57,42 @@ public class GlobalExceptionHandler {
                 .body(data);
     }
 
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException exception) {
+        log.warn("BAD_REQUEST_EXCEPTION :: message = {}", exception.getMessage());
+
+        ExceptionResponse data = new ExceptionResponse(exception.getMessage());
+        return ResponseEntity.badRequest()
+                .body(data);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ExceptionResponse> handleMissingParam(MissingServletRequestParameterException exception) {
+        ExceptionResponse data = new ExceptionResponse(exception.getMessage());
+        return ResponseEntity.badRequest()
+                .body(data);
+    }
+
+    @ExceptionHandler(org.springframework.http.converter.HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(
+            org.springframework.http.converter.HttpMessageNotReadableException exception
+    ) {
+        String message = "잘못된 요청입니다. (파라미터 바인딩 실패, 바디 누락 등)";
+
+        log.warn("HTTP_MESSAGE_NOT_READABLE_EXCEPTION :: message = {}", exception.getMessage());
+
+        ExceptionResponse data = new ExceptionResponse(message);
+        return ResponseEntity.badRequest().body(data);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodNotAllowed(HttpRequestMethodNotSupportedException exception) {
+        String message = "지원하지 않는 HTTP 메서드입니다.";
+
+        log.warn("HTTP_MESSAGE_NOT_READABLE_EXCEPTION :: message = {}", exception.getMessage());
+
+        ExceptionResponse data = new ExceptionResponse(message);
+        return ResponseEntity.badRequest().body(data);
+
+    }
 }
